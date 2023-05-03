213a47e71027e5a29997ed97b4f422c711f84544
Pavel Kulik <Kanstantin0105@gmail.com>
2023-05-03 21:37:59 +0300

    Add task_1

---
+++ task_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool isFiveDigitNumber(int date)
+        {
+            if (date.ToString().Length == 5) return true;
+            else return false;
+        }
+
+        int inputDate(string massege)
+        {
+            Console.Write(massege);
+            int date = int.Parse(Console.ReadLine()!);
+            if (isFiveDigitNumber(date))
+                return date;
+            else
+                return -1;
+        }
+
+        bool isPalindrome(int date)
+        {
+            string dateString = date.ToString();
+            for (int i = 0; i < dateString.Length / 2; i++)
+            {
+                if (dateString[i] != dateString[dateString.Length - 1 - i])
+                    return false;
+            }
+
+            return true;
+        }
+
+        int number = inputDate("Введите пятизначное число: ");
+        if (number != -1)
+        {
+            Console.Write(number + " -> ");
+            if (isPalindrome(number))
+                Console.WriteLine("да");
+            else
+                Console.WriteLine("нет");
+        }
+        else
+            Console.WriteLine("Введеное число не пятизначное!");
+    }
+}
